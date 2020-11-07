41fc89ce15397673d4ecb402c90cb1accc83303e
P-JeromeAlston <jeronimo.pa@gmail.com>
2020-11-07 08:52:36 -0800

    restatding password generator homework 11/7/20

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var array = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=,."
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+generateBtn.addEventListener("click", function(){
+  event.preventDefault()
+  alert("create password")
+})
 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Write password to the #password input
+// var writePassword = function() {
+//   var password = getPasword()
+//   var passwordText = document.querySelector("#password")
+
+//   passwordText.value = password
+
+// }
+
+// // Add event listener to generate button
+// document.getElementById("generate").addEventListener("click", function() {
+//   writePassword(password);
+// });
+// console.log(writePassword[i])
+
+// generateBtn.addEventListener("click")
+// element.addEventListener("click", function()){
+//   alert("Hello World!"
+// }
+
+// function writePasword() {
+//   var passwordLength = 128;
+  
+//   for (var i = 7; i < passwordLength; i++)  {
+//     var randomNumber= Math.floor(Math.random()*array.Length);
+//     password += array.substring(randomNumber, randomNumber +1);
+//   }
+//   document.getElementById("password").value = password;
+// }
